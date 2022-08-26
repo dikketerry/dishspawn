2fa15aeb83935396c3b9b29d86208463d23b0980
erikh <91531357+erikholl@users.noreply.github.com>
2022-08-26 21:29:43 +0200

    recipe page acceptable

---
+++ src/main/java/io/eho/dishspawn/play/RecipeInit.java
@@ -0,0 +1,22 @@
+package io.eho.dishspawn.play;
+
+public class RecipeInit {
+
+    public static void main(String[] args) {
+
+        // create ingredients
+
+        // save ingredients to DB
+
+        // create recipe
+
+        // create recipe-ingredients
+
+        // optional: create / assign visual recipe
+
+        // save recipe, cascading to recipe-ingredients and visual
+
+        // optional: generate (or just assign) visual to recipe
+
+    }
+}
