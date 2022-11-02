e879a07ff61a09d5e226446c58268c5ced5bf449
erikh <91531357+erikholl@users.noreply.github.com>
2022-11-02 20:21:33 +0100

    security works! details implementation still needed

--- src/main/java/io/eho/dishspawn/config/SecurityConfiguration.java
+++ src/main/java/io/eho/dishspawn/config/SecurityConfiguration.java
@@ -14,20 +14,23 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.httpBasic()
                 .and()
                 .authorizeRequests()
                 .mvcMatchers("/chef/**", "/home", "recipe**", "/search**", "/spawn**", "/visual**").permitAll()
-                .mvcMatchers("/ingredient/all").hasRole("chef")
+                .mvcMatchers("/recipe/add", "/loveVisual").hasAnyRole("chef", "superchef", "admin")
+                .mvcMatchers("/ingredient/add").hasAnyRole("superchef", "admin")
+                .mvcMatchers("/api/**", "/ingredient/all", "/chef/all", "/recipe/all").hasRole("admin")
+//                .and().formLogin().loginPage("/sign-in").permitAll()
+//                .and().logout().logoutUrl("/sign-out").permitAll()
                 .and().build();
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
+
 }
