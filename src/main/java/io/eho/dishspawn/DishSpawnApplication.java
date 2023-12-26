f20574fa4717a38aa6746fe7b7360da20305acb5
erikh <91531357+erikholl@users.noreply.github.com>
2023-12-26 12:30:17 +0100

    test commit

--- src/main/java/io/eho/dishspawn/DishSpawnApplication.java
+++ src/main/java/io/eho/dishspawn/DishSpawnApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DishSpawnApplication {
+	// test comment to test github commit
 
 	public static void main(String[] args) {
 		SpringApplication.run(DishSpawnApplication.class, args);
